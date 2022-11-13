a7df0c50f6d3aee4b4a545dd3e850d11d4bc465c
root <elpatoninjasuperninja@gmail.com>
2022-11-13 15:35:01 -0800

    adding main.h structure style, a little more concise than printf_h

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+/**
+ * struct formatFunctions - contains the formats
+ * @specifier: format specifier
+ * @f: pointer to formatting functions
+ */
+struct formatFunctions
+{
+	char specifier;
+	int (*f)(va_list args, int base, char *buffer, int indicator);
+};
+int _printf(const char *format, ...);
+int (*formatFunc(char c))(va_list, int base, char *buf, int ind);
+int PosNtoString(long long int number, int base, char *buffer, int indicator);
+int ntostring(va_list args, int base, char *buffer, int indicator);
+int _puts(va_list args, int base, char *buffer, int indicator);
+int _putchar(va_list args, int base, char *buffer, int indicator);
+int printPercentage(va_list __attribute__ ((unused)), int, char *, int);
+
+#endif
