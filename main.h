3699d9604fdb7d24704b3a3d241ab794526e5763
root <elpatoninjasuperninja@gmail.com>
2022-11-13 16:08:19 -0800

    typedefed the format getter

--- main.h
+++ main.h
@@ -11,14 +11,14 @@
  * @specifier: format specifier
  * @f: pointer to formatting functions
  */
-struct formatFunctions
+typedef struct formatFunctions
 {
 	char specifier;
 	int (*f)(va_list args, int base, char *buffer, int indicator);
-};
+}formatFunctions;
 int _printf(const char *format, ...);
 int (*formatFunc(char c))(va_list, int base, char *buf, int ind);
-int PosNtoString(long long int number, int base, char *buffer, int indicator);
+int PosNtoString(long int number, int base, char *buffer, int indicator);
 int ntostring(va_list args, int base, char *buffer, int indicator);
 int _puts(va_list args, int base, char *buffer, int indicator);
 int _putchar(va_list args, int base, char *buffer, int indicator);
