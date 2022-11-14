6a2facb245ab54a47e5ad1726b29971d6ed0a138
root <elpatoninjasuperninja@gmail.com>
2022-11-13 16:03:43 -0800

    puts the character into the sceen

--- _puts.c
+++ _puts.c
@@ -3,22 +3,25 @@
 
 /**
  * _puts - Prints a string to the stdout
- * @string: String to be printed
+ * @args: String to be printed
+ * @b: unused
+ * @buf: unused
+ * @ind: unused
  * Return: Returns number of prints
  */
 
 int _puts(va_list args, int b, char *buf, int ind)
 {
-  char *string = va_arg(args, char *);
-  int trash = b = ind = 0;
-  int i;
-  for (i = 0; buf[i]; i++)
-    buf[i] = 0;
+	char *string = va_arg(args, char *);
+	int trash = b = ind = 0;
+	int i;
 
-  if (!(string))
-    return (-1);
-  for (i = 0; string[i]; i++)
-    putchar(string[i]);
-  return (i + trash);
+	for (i = 0; buf[i]; i++)
+		buf[i] = 0;
+	if (!(string))
+		return (-1);
+	for (i = 0; string[i]; i++)
+		putchar(string[i]);
+	return (i + trash);
 }
 
